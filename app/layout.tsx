855072458f00a888ccc488fb396cc43e93030b44
Zoltara <208447823+Zoltara@users.noreply.github.com>
2026-01-16 06:19:40 +0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,2 @@
-export default function RootLayout({ children, }: { children: React.ReactNode }) { return ( <html lang="en"> <body>{children}</body> </html> ) } 
-
-
+import './globals.css'; export default function RootLayout({ children, }: { children: React.ReactNode }) 
+{ return ( <html lang="en"> <body>{children}</body> </html> ) }
--- next.config.js
+++ next.config.js
@@ -1,2 +1,1 @@
-/** @type {import('next').NextConfig} */ 
-const nextConfig = { typescript: { ignoreBuildErrors: true, }, } module.exports = nextConfig  
+/** @type {import('next').NextConfig} */ const nextConfig = { typescript: { ignoreBuildErrors: true, }, eslint: { ignoreDuringBuilds: true, } } module.exports = nextConfig
