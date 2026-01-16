6c7a72a24fda0c0581b3aa720cbe27cf7115ebfe
Zoltara <208447823+Zoltara@users.noreply.github.com>
2026-01-16 08:33:33 +0700

    Delete app directory

--- app/layout.tsx
+++
@@ -1,1 +0,0 @@
-import './globals.css' export default function RootLayout({ children, }: { children: React.ReactNode }) { return ( <html lang="en"> <body className="bg-zinc-950"> {children} </body> </html> ) } 
